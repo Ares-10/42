46c83b625912ed1e62e43fc20e0f48705fcac22a
Hyungeun Cho <hyungcho@c3r4s5.42seoul.kr>
2023-11-15 18:59:26 +0900

    fix: 메모리 오류

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,14 +6,21 @@
 /*   By: hyungcho <hyungcho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 18:24:19 by hyungcho          #+#    #+#             */
-/*   Updated: 2023/11/15 18:24:57 by hyungcho         ###   ########.fr       */
+/*   Updated: 2023/11/15 18:54:28 by hyungcho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include "get_next_line.h"
 
-char	*gnl_read_line(int fd, char *buf, char *backup)
+static char	*gnl_free(char **str)
+{
+	free(*str);
+	*str = 0;
+	return (NULL);
+}
+
+static char	*gnl_read_line(int fd, char *buf, char *backup)
 {
 	int		read_size;
 	char	*temp;
@@ -22,10 +29,7 @@ char	*gnl_read_line(int fd, char *buf, char *backup)
 	{
 		read_size = read(fd, buf, BUFFER_SIZE);
 		if (read_size == -1 || (read_size == 0 && !backup[0]))
-		{
-			free(backup);
-			return (NULL);
-		}
+			return (gnl_free(&backup));
 		if (read_size == 0)
 			break ;
 		buf[read_size] = '\0';
@@ -40,28 +44,26 @@ char	*gnl_read_line(int fd, char *buf, char *backup)
 	return (backup);
 }
 
-char	*gnl_get_line(char *backup)
+static char	*gnl_get_line(char **backup)
 {
 	int		i;
 	char	*line;
 
 	i = 0;
-	while (backup[i] && backup[i] != '\n')
+	while ((*backup)[i] && (*backup)[i] != '\n')
 		i++;
-	if (backup[i] == '\n')
+	if ((*backup)[i] == '\n')
 		i++;
-	line = gnl_substr(backup, 0, i);
+	line = gnl_substr((*backup), 0, i);
 	if (!line || line[0] == '\0')
 	{
-		if (line)
-			free(line);
-		free(backup);
-		return (NULL);
+		free(line);
+		return (gnl_free(backup));
 	}
 	return (line);
 }
 
-char	*gnl_update_backup(char *backup, char *line)
+static char	*gnl_update_backup(char *backup, char *line)
 {
 	char	*temp;
 	int		line_len;
@@ -70,11 +72,8 @@ char	*gnl_update_backup(char *backup, char *line)
 	temp = backup;
 	backup = gnl_substr(temp, line_len, gnl_strlen(temp) - line_len);
 	free(temp);
-	if (backup[0] == '\0')
-	{
-		free(backup);
-		return (NULL);
-	}
+	if (!backup || backup[0] == '\0')
+		return (gnl_free(&backup));
 	return (backup);
 }
 
@@ -92,15 +91,12 @@ char	*get_next_line(int fd)
 		return (NULL);
 	buf = (char *)malloc(BUFFER_SIZE + 1);
 	if (!buf)
-	{
-		free(backup);
-		return (NULL);
-	}
+		return (gnl_free(&backup));
 	backup = gnl_read_line(fd, buf, backup);
 	free(buf);
 	if (!backup)
 		return (NULL);
-	line = gnl_get_line(backup);
+	line = gnl_get_line(&backup);
 	if (!line)
 		return (NULL);
 	backup = gnl_update_backup(backup, line);
--- get_next_line/get_next_line_bonus.c
+++ get_next_line/get_next_line_bonus.c
@@ -6,13 +6,20 @@
 /*   By: hyungcho <hyungcho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 18:24:19 by hyungcho          #+#    #+#             */
-/*   Updated: 2023/11/15 18:24:15 by hyungcho         ###   ########.fr       */
+/*   Updated: 2023/11/15 18:56:30 by hyungcho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <limits.h>
 #include "get_next_line_bonus.h"
+
+static char	*gnl_free(char **str)
+{
+	free(*str);
+	*str = 0;
+	return (NULL);
+}
 
 static char	*gnl_read_line(int fd, char *buf, char *backup)
 {
@@ -23,10 +30,7 @@ static char	*gnl_read_line(int fd, char *buf, char *backup)
 	{
 		read_size = read(fd, buf, BUFFER_SIZE);
 		if (read_size == -1 || (read_size == 0 && !backup[0]))
-		{
-			free(backup);
-			return (NULL);
-		}
+			return (gnl_free(&backup));
 		if (read_size == 0)
 			break ;
 		buf[read_size] = '\0';
@@ -41,23 +45,21 @@ static char	*gnl_read_line(int fd, char *buf, char *backup)
 	return (backup);
 }
 
-static char	*gnl_get_line(char *backup)
+static char	*gnl_get_line(char **backup)
 {
 	int		i;
 	char	*line;
 
 	i = 0;
-	while (backup[i] && backup[i] != '\n')
+	while ((*backup)[i] && (*backup)[i] != '\n')
 		i++;
-	if (backup[i] == '\n')
+	if ((*backup)[i] == '\n')
 		i++;
-	line = gnl_substr(backup, 0, i);
+	line = gnl_substr((*backup), 0, i);
 	if (!line || line[0] == '\0')
 	{
-		if (line)
-			free(line);
-		free(backup);
-		return (NULL);
+		free(line);
+		return (gnl_free(backup));
 	}
 	return (line);
 }
@@ -71,11 +73,8 @@ char	*gnl_update_backup(char *backup, char *line)
 	temp = backup;
 	backup = gnl_substr(temp, line_len, gnl_strlen(temp) - line_len);
 	free(temp);
-	if (backup[0] == '\0')
-	{
-		free(backup);
-		return (NULL);
-	}
+	if (!backup || backup[0] == '\0')
+		return (gnl_free(&backup));
 	return (backup);
 }
 
@@ -93,15 +92,12 @@ char	*get_next_line(int fd)
 		return (NULL);
 	buf = (char *)malloc(BUFFER_SIZE + 1);
 	if (!buf)
-	{
-		free(backup);
-		return (NULL);
-	}
+		return (gnl_free(&backup[fd]));
 	backup[fd] = gnl_read_line(fd, buf, backup[fd]);
 	free(buf);
 	if (!backup[fd])
 		return (NULL);
-	line = gnl_get_line(backup[fd]);
+	line = gnl_get_line(&backup[fd]);
 	if (!line)
 		return (NULL);
 	backup[fd] = gnl_update_backup(backup[fd], line);
