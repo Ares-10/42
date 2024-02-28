fbdf33f70c4ac29ff002ab3bd793da3c55b1492e
Ares-10 <cholionking1@naver.com>
2024-02-28 20:30:07 +0900

    chore: ft_printf.h

--- pipex/ft_printf/ft_printf.h
+++ pipex/ft_printf/ft_printf.h
@@ -3,22 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hyungcho <hyungcho@student.42.fr>          +#+  +:+       +#+        */
+/*   By: johyeongeun <johyeongeun@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 17:23:54 by hyungcho          #+#    #+#             */
-/*   Updated: 2023/12/22 15:45:26 by hyungcho         ###   ########.fr       */
+/*   Updated: 2024/02/28 20:28:15 by johyeongeun      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-int	ft_printf(const char *str, ...);
+# include "ft_printf_utils.h"
 
-int	ft_prf_putchar(char c, int *len);
-int	ft_prf_putstr(char *s, int *len);
-int	ft_prf_putnbr(int n, int *len);
-int	ft_prf_putnbr_1(long n, int *len);
-int	ft_prf_itohex(int n, char hex_string[], int *len, int flag);
+int	ft_printf(const char *str, ...);
 
 #endif
