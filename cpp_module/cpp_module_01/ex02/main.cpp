d3140e7d3d3aaa2003659acfe8677f6a8cfbd10c
Ares-10 <cholionking1@naver.com>
2024-07-30 22:49:51 +0900

    feat: HI THIS IS BRAIN (cpp_module_01/ex02)

---
+++ cpp_module/cpp_module_01/ex02/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+	std::string string = "HI THIS IS BRAIN";
+	std::string *stringPTR = &string;
+	std::string &stringREF = string;
+
+	std::cout << "address of string: " << &string << std::endl;
+	std::cout << "stringPTR: " << stringPTR << std::endl;
+	std::cout << "stringREF: " << &stringREF << std::endl;
+
+	std::cout << "value of string: " << string << std::endl;
+	std::cout << "value pointed to by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "value pointed to by stringREF: " << stringREF << std::endl;
+}
