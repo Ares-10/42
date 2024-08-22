07c2df55c7f7c6376d759760b5f83ef99f0e21b4
Ares-10 <cholionking1@naver.com>
2024-08-23 01:22:41 +0900

    feat: Aaaaand... OPEN (cpp_module03/ex00)

---
+++ cpp_module/cpp_module_03/ex00/ClapTrap.cpp
@@ -0,0 +1,80 @@
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap() : _hit_points(10), _energy_points(10), _attack_damage(0)
+{
+}
+
+ClapTrap::ClapTrap(const std::string &name) : _name(name), _hit_points(10), _energy_points(10), _attack_damage(0)
+{
+	std::cout << "String constructor called" << std::endl;
+}
+
+ClapTrap::ClapTrap(const ClapTrap &copy)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = copy;
+}
+
+ClapTrap::~ClapTrap()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+ClapTrap &ClapTrap::operator=(const ClapTrap &src)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	_name = src._name;
+	_hit_points = src._hit_points;
+	_energy_points = src._energy_points;
+	_attack_damage = src._attack_damage;
+	return *this;
+}
+
+bool ClapTrap::statusCheck() const
+{
+	if (_hit_points <= 0)
+	{
+		std::cout << "ClapTrap " << _name << " can't do anything. Becaues it's hit points is 0" << std::endl;
+		return false;
+	}
+	if (_energy_points <= 0)
+	{
+		std::cout << "ClapTrap " << _name << " can't do anything, becaues it's energy points is 0" << std::endl;
+		return false;
+	}
+	return true;
+}
+
+void ClapTrap::attack(const std::string &target)
+{
+	if (!statusCheck())
+		return;
+	_energy_points--;
+	std::cout << "ClapTrap " << _name << " attacks " << target
+			<< ", causing " << _attack_damage << " points of damage!" << std::endl;
+}
+
+void ClapTrap::takeDamage(unsigned int amount)
+{
+	if (_hit_points == 0)
+	{
+		std::cout << "ClapTrap " << _name << " can't take damage, because it's already dead" << std::endl;
+		return;
+	}
+	if (_hit_points < amount)
+		_hit_points = 0;
+	else
+		_hit_points -= amount;
+	std::cout << "ClapTrap " << _name << " took damage and lost " << amount << " energy points,"
+			<< " so it's energy point is now " << _hit_points << std::endl;
+}
+
+void ClapTrap::beRepaired(unsigned int amount)
+{
+	if (!statusCheck())
+		return;
+	_energy_points--;
+	_hit_points += amount;
+	std::cout << "ClapTrap " << _name << " is repaired and gained " << amount << " energy points, "
+			<< "so it's energy point is now " << _hit_points << std::endl;
+}
---
+++ cpp_module/cpp_module_03/ex00/main.cpp
@@ -0,0 +1,15 @@
+#include "ClapTrap.hpp"
+
+int main()
+{
+	ClapTrap a("tom");
+	ClapTrap b("bob");
+	ClapTrap c("amy");
+
+	a.attack("bob");
+	b.takeDamage(15);
+	c.beRepaired(4);
+
+	b.takeDamage(3);
+	b.beRepaired(3);
+}
