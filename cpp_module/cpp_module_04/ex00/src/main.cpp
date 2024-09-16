c50337bca4d86276926c749e887ed5a3758f5db1
Ares-10 <cholionking1@naver.com>
2024-09-16 17:40:47 +0900

    chore: 테스트 코드 주석처리

--- cpp_module/cpp_module_04/ex00/src/main.cpp
+++ cpp_module/cpp_module_04/ex00/src/main.cpp
@@ -4,8 +4,14 @@
 #include "../include/WrongAnimal.hpp"
 #include "../include/WrongCat.hpp"
 
+// void f()
+// {
+//  	system("leaks Animal");
+// }
+
 int main()
 {
+	// atexit(f);
 	const Animal* meta = new Animal();
 	const Animal* j = new Dog();
 	const Animal* i = new Cat();
@@ -14,6 +20,9 @@ int main()
 	i->makeSound(); //will output the cat sound!
 	j->makeSound();
 	meta->makeSound();
+	delete meta;
+	delete i;
+	delete j;
 
 	std::cout << std::endl;
 
@@ -22,5 +31,8 @@ int main()
 	std::cout << i2->getType() << " " << std::endl;
 	i2->makeSound(); //will output the cat sound!
 	meta2->makeSound();
+	delete meta2;
+	delete i2;
+
 	return 0;
 }
