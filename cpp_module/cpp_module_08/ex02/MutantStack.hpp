9693660e53e59b827c6321760c368320d9d81efc
Ares-10 <cholionking1@naver.com>
2024-11-18 19:56:17 +0900

    feat: cpp08/ex02 mutantstack

---
+++ cpp_module/cpp_module_08/ex02/MutantStack.hpp
@@ -0,0 +1,44 @@
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+# include <stack>
+
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+public:
+	MutantStack() : std::stack<T>()
+	{
+	}
+
+	MutantStack(MutantStack const &src)
+	{
+		*this = src;
+	}
+
+	~MutantStack()
+	{
+	}
+
+	MutantStack &operator=(MutantStack const &src)
+	{
+		if (this == &src)
+			return *this;
+		this->c = src.c;
+		return *this;
+	}
+
+	typedef typename std::stack<T>::container_type::iterator iterator;
+
+	iterator begin()
+	{
+		return std::stack<T>::c.begin();
+	}
+
+	iterator end()
+	{
+		return std::stack<T>::c.end();
+	}
+};
+
+#endif //MUTANTSTACK_HPP
