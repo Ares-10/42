bbcef0003f1c969931e219060c437afc79afe8c2
Ares-10 <cholionking1@naver.com>
2024-11-24 02:10:31 +0900

    fix: iostream include 위치

--- cpp_module/cpp_module_08/ex00/easyfind.hpp
+++ cpp_module/cpp_module_08/ex00/easyfind.hpp
@@ -1,6 +1,8 @@
 #ifndef EASYFIND_HPP
 # define EASYFIND_HPP
 
+#include <iostream>
+
 template <typename T>
 int easyfind(T &t, int n)
 {
--- cpp_module/cpp_module_08/ex00/main.cpp
+++ cpp_module/cpp_module_08/ex00/main.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include "easyfind.hpp"
 
 int main()
