e4ba9f264260d5a498e6eaeffd41eb62c03796a6
Ares-10 <cholionking1@naver.com>
2025-02-03 16:11:26 +0900

    fix: wp cli add admin

--- inception/srcs/requirements/wordpress/tools/script.sh
+++ inception/srcs/requirements/wordpress/tools/script.sh
@@ -14,9 +14,9 @@ mv wp-cli.phar /usr/local/bin/wp
 
 wp core download --allow-root
 
-wp user create ${WP_ADMIN_USER} ${WP_ADMIN_EMAIL} --role=author --user_pass=${WP_ADMIN_PWD} --allow-root
+wp core install --url=${DOMAIN_NAME}/ --title=${WP_TITLE} --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PWD} --admin_email=${WP_ADMIN_EMAIL} --skip-email --allow-root
 
-wp user create ${WP_USR} ${WP_EMAIL} --role=author --user_pass=${WP_PWD} --allow-root
+wp user create ${WP_USER} ${WP_EMAIL} --role=author --user_pass=${WP_PWD} --allow-root
 
 sed -i 's/listen = \/run\/php\/php7.4-fpm.sock/listen = 0.0.0.0:9000/g' /etc/php/7.4/fpm/pool.d/www.conf
 
